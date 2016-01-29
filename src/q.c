ca5572012e1261f358f3e4c306053b82ed71579d
Sebastian Klatt <sebastian@markow.io>
2016-01-29 01:25:32 +0100

    Stage

--- src/q.c
+++ src/q.c
@@ -1,20 +1,19 @@
 #define _POSIX_SOURCE
-#define _POSIX_C_SOURCE 200809L
+#define _POSIX_C_SOURCE 199309L
 
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/time.h>
 
 #include <errno.h>
 #include <fcntl.h>
+#include <inttypes.h>
+#include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-
-
-#define Q_DEFAULT_PATH ".q"
-#define Q_PATH_ENV "QPATH"
-#define HOME_ENV "HOME"
-
 
 #ifdef PRINT_DEBUG
 #define DEBUG(...) fprintf(stdout, __VA_ARGS__)
@@ -23,31 +22,54 @@
 #endif
 #define ERROR(...) fprintf(stderr, __VA_ARGS__)
 
+const char *Q_DEFAULT_PATH = ".q";
+const char *Q_PATH_ENV = "QPATH";
+const char *HOME_PATH_ENV = "HOME";
+
+struct child_proc {
+	pid_t pid;
+	bool monitorProc;
+	bool killProc;
+	int64_t timestamp;
+};
+
 static void usage(void) {
 	printf("Usage: q\n");
 }
 
 char *getQPath(void) {
-	char *path = getenv(Q_PATH_ENV);
-	if (path) {
-		DEBUG("test")
+	char *path = malloc(sizeof(char) * 500);
+	if (!path) {
+		ERROR("could not allocate memory");
+		return NULL;
+	}
+
+	char *custom_path = getenv(Q_PATH_ENV);
+	if (custom_path) {
+		strcpy(path, custom_path);
+		DEBUG("return custom"); // XXX(SK)
 		return path;
 	}
 
-	char *home_path = getenv(HOME_ENV);
+	char *home_path = getenv(HOME_PATH_ENV);
 	if (!home_path) {
-		return path;
+		ERROR("could not determin home directory"); // XXX(SK)
+		return NULL;
 	}
 
-	path = (char *)realloc(path, sizeof(char) * 500);
-	snprintf(path, 500, "%s/%s", home_path, Q_DEFAULT_PATH);
-
+	sprintf(path, "%s/%s", home_path, Q_DEFAULT_PATH);
+	DEBUG("return default"); // XXX(SK)
 	return path;
 }
 
 int main(int argc, char **argv) {
 	int opt = 0;
 	int qpath_fd = 0;
+	int pipe_fd[2];
+	struct timeval timestamp;
+	struct child_proc *child;
+
+	gettimeofday(&timestamp, NULL);
 
 	while ((opt = getopt(argc, argv, "")) != -1) {
 		switch (opt) {
@@ -59,29 +81,55 @@ int main(int argc, char **argv) {
 	if (argc < 2) {
 	usage:
 		usage();
-		exit(1);
+		return 1;
 	}
 
 	char *qpath = getQPath();
 	if (!qpath) {
-		perror("Could not determin queue path");
-		exit(1);
+		ERROR("Could not determin queue path");
+		return 1;
 	}
+
+	printf("path: %s\n", qpath);
 
 	if (mkdir(qpath, 0700) < 0) {
 		if (errno != EEXIST) {
 			perror("Could not create queue path");
-			exit(200); // XXX(SK): Error code?
+			return 200; // XXX(SK): Error code?
 		}
 	}
 
 	qpath_fd = open(qpath, O_RDONLY);
 	if (qpath_fd < 0) {
 		perror("Could not open queue path");
-		exit(200); // XXX(SK): Error code?
+		return 200; // XXX(SK): Error code?
 	}
 
-	printf("%s\n", qpath);
+	free(qpath);
+	qpath = NULL;
+
+	if (pipe(pipe_fd) < 0) {
+		perror("Could not init pipe");
+		return 200; // XXX(SK): Error code?
+	}
+
+	pid_t pid = fork();
+
+	if (pid > 0) {
+		child = malloc(sizeof(struct child_proc));
+		child->pid = pid;
+		child->timestamp = (int64_t)timestamp.tv_sec*1000 + timestamp.tv_usec/1000;
+
+		printf("child pid: %d\n", child->pid);
+		printf("child timestamp: %lld\n", child->timestamp);
+
+		free(child);
+		child = NULL;
+	} else {
+		printf("parent pid: %d\n", pid);
+	}
+
+	close(qpath_fd);
 
 	return 0;
 }
